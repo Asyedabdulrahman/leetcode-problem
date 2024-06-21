001bb8005fa749604e12f60ffd404c76b0ce4650
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2024-06-22 01:20:46 +0530

    Time: 61 ms (56.49%) | Memory: 51 MB (64.67%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -4,10 +4,9 @@
  */
 var singleNumber = function(nums) {
     let xor = 0;
-    for(let i = 0;i< nums.length;i++){
-        xor = xor ^ nums[i];
-    }
+    for(let num of nums)
+        xor = xor ^ num;
+    
     return xor;
-    
-    
+     
 };
