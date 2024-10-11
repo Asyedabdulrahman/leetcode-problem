6523e018dc0ce138cea4f3c7e86bef221fad0d1d
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2024-10-11 11:04:26 +0530

    Time: 61 ms (56.62%) | Memory: 51.2 MB (82.84%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,14 +3,18 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let cnt=0,n=nums.length,ele=0;
-    for(let i=0;i<n;i++){
-        if(cnt==0){
-            cnt=1;
-            ele=nums[i];
+    let element = null
+    let count = 0
+    for(let i=0; i< nums.length; i++) {
+        if(count === 0) {
+            element = nums[i]
         }
-        else if(nums[i]==ele) cnt++;
-        else cnt--;
+        if(element === nums[i]) {
+            count++
+        } else {
+            count--
+        }
+        
     }
-    return ele;
+    return element
 };
