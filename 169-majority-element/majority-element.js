29cf00c826fd1d0d0246e79c70fb42f055e0877f
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2024-10-11 11:04:04 +0530

    Time: 49 ms (95.14%) | Memory: 50.8 MB (95.52%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,38 +3,14 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let n = nums.length;
-    let cnt = 0; 
-    let ele = 0;
-
-    //find the suitable candidate for majority element finding 
-    for(let i = 0 ; i < n; i++){
-        if(cnt === 0){
-            cnt = 1;
-            ele = nums[i];
+    let cnt=0,n=nums.length,ele=0;
+    for(let i=0;i<n;i++){
+        if(cnt==0){
+            cnt=1;
+            ele=nums[i];
         }
-        else if (nums[i] === ele){
-            cnt++;
-        }
-        else{
-            cnt--;
-        }
+        else if(nums[i]==ele) cnt++;
+        else cnt--;
     }
-
-    //finding the majority element based on element selection 
-
-    let cnt1 = 0;
-    for(let i = 0;  i < n; i++){
-        if(nums[i] === ele){
-            cnt1++;
-        }
-    }
-
-    if(cnt1 > Math.floor(n/2)){
-        return ele
-    }
-    else{
-        return -1
-    }
-    
+    return ele;
 };
