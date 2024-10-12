38029d6cff6226e44dcd8cfda64b98040a5ba57b
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2024-10-12 18:00:09 +0530

    Time: 78 ms (39.48%) | Memory: 58.7 MB (83.30%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let maxProfit = 0;
+    let n = prices.length;
+    let minPrice = Infinity;
+
+    for(let i = 0 ; i < n;  i++){
+        if(prices[i] < minPrice){
+            minPrice = prices[i];
+        }
+
+        let profit = prices[i] - minPrice;
+
+        if(profit > maxProfit){
+            maxProfit = profit;
+        }
+    }
+    return maxProfit;
+};
