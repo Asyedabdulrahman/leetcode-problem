77cd82da1425c57bcee06fffb7dd467989aeb589
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2024-12-01 21:18:59 +0530

    Time: 5 ms (31.37%) | Memory: 53.8 MB (44.71%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -2,29 +2,34 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var setZeroes = function(matrix) {
+var setZeroes = function (matrix) {
     const n = matrix.length;
     const m = matrix[0].length;
-    const row = new Array(m).fill(0);
-    const col = new Array(m).fill(0);
+    const row = new Array(n).fill(0); // row array
+    const col = new Array(m).fill(0); // col array
 
-    //traverse the matrix
-    for(let i = 0; i < n;i++){
-        for(let j = 0; j < m; j++){
-            if(matrix[i][j] === 0){
+    // Traverse the matrix:
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
+            if (matrix[i][j] === 0) {
+                // mark ith index of row with 1:
                 row[i] = 1;
+
+                // mark jth index of col with 1:
                 col[j] = 1;
             }
         }
     }
 
-    for(let i = 0; i < n; i++){
-        for(let j = 0;j < m; j++){
-            if(row[i] || col[j]){
+    // Finally, mark all (i, j) as 0
+    // if row[i] or col[j] is marked with 1.
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
+            if (row[i] || col[j]) {
                 matrix[i][j] = 0;
             }
         }
     }
+
     return matrix;
-    
 };
