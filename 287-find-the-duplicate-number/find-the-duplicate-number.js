5841aea05e802a3210bf07f04bd7ff83f46c2259
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2024-12-01 13:23:41 +0530

    Time: 122 ms (12.38%) | Memory: 61 MB (40.86%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/find-the-duplicate-number.js
@@ -3,12 +3,10 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-    let seen = {}; // Object to track seen numbers
-    for (let i = 0; i < nums.length; i++) {
-        if (seen[nums[i]]) {
-            return nums[i]; // Duplicate found
+    nums.sort((a,b) => a - b);
+    for(let i = 0; i < nums.length;i++){
+        if(nums[i] == nums[i+1]){
+            return nums[i];
         }
-        seen[nums[i]] = true; // Mark the number as seen
-    }
-    return -1; // No duplicate found
+    }    
 };
