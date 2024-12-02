c28d92e2b37d50f477fee2e4b2cb2a9f5ababb51
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2024-12-03 00:47:56 +0530

    Time: 9 ms (48.33%) | Memory: 51.9 MB (14.83%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,24 +4,24 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let numswithindex = nums.map((num,index)=> [num,index]);
-    numswithindex.sort((a,b) => a[0] - b[0]);
+  let numswithIndex = nums.map((num,index) => [num,index]);
+  numswithIndex.sort((a,b) => a[0] - b[0]);
 
-    let left = 0;
-    let right = numswithindex.length - 1;
 
-    while(left < right) {
-        let sum = numswithindex[left][0] + numswithindex[right][0];
-        if(sum == target) { 
-            return [numswithindex[left][1] , numswithindex[right][1]];
-        }
-        else if(sum < target){
-            left++
-        }
-        else{
-            right--
-        }
+  let left = 0; 
+  let right = numswithIndex.length - 1 ;
+
+  while(left < right) {
+    let sum = numswithIndex[left][0] + numswithIndex[right][0];
+    if(sum == target) { 
+        return [numswithIndex[left][1] , numswithIndex[right][1]];
     }
-    return [];
-    
+    else if (sum < target){
+        left++;
+    }
+    else{ 
+        right--;
+    }
+  }
+  return [];
 };
