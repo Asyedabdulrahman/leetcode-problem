87a8a230a9835ed5a9c7627c6c7d86dfbc985ccc
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2024-12-15 12:55:59 +0530

    Time: 14 ms (84.80%) | Memory: 93.7 MB (46.04%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.js
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.js
@@ -3,21 +3,20 @@
  * @return {number[]}
  */
 var rearrangeArray = function(nums) {
-    let positive = 1;
-    let negative = 0;
     let n = nums.length;
+    if(n == 0) return 0;//base case
     let tempArray = new Array(n).fill(0);
+    let positive = 1, negative = 0
 
     for(let i = 0; i < n; i++){
         if(nums[i] < 0){
             tempArray[positive] = nums[i];
-            positive+=2;
+            positive+= 2;
         }
         else{
             tempArray[negative] = nums[i];
-            negative+=2;
+            negative+= 2;
         }
     }
     return tempArray;
-    
 };
