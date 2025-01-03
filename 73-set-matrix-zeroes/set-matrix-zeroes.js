1e73f1cf1e225e644c13f7d9517160000636af93
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2025-01-03 11:53:01 +0530

    Time: 3 ms (58.31%) | Memory: 53.4 MB (75.80%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -3,24 +3,27 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-    const zeros = []
-    const rowSet = new Set()
-    const colSet = new Set()
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[i].length; j++) {
-            if (matrix[i][j] === 0) zeros.push([i, j])
+    let n = matrix.length;
+    let m = matrix[0].length;
+    let row = new Array(n).fill(0);
+    let column = new Array(m).fill(0);
+
+    for(let i = 0; i < n; i++){
+        for(let j = 0 ; j < m; j++){
+            if(matrix[i][j] === 0){
+                row[i] = 1;
+                column[j] = 1;
+            }
         }
     }
-    const rowSeen = new Set()
-    const colSeen = new Set()
-    for (const zero of zeros) {
-        if (!rowSeen.has(zero[1])) {
-            for (let i = 0; i < matrix.length; i++) matrix[i][zero[1]] = 0
-            rowSeen.add(zero[1])
+
+
+    for(let i = 0 ; i < n; i++){
+        for(let j =0; j < m; j++){
+          if(row[i] || column[j] == 1){
+            matrix[i][j] = 0;
+            }
         }
-        if (!colSeen.has(zero[0])) {
-            for (let i = 0; i < matrix[0].length; i++) matrix[zero[0]][i] = 0
-            colSeen.add(zero[0])
-        } 
     }
+    return matrix;
 };
