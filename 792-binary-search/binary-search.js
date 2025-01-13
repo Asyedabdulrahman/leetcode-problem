e214b158d672c87d3b21746434eeef78df7de059
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2025-01-13 18:58:04 +0530

    Time: 0 ms (100.00%) | Memory: 53.3 MB (20.89%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -4,11 +4,19 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let n = nums.length; 
+    let left = 0;
+    let right = nums.length - 1;
 
-    for(let i = 0 ; i < n; i++){
-        if(nums[i] == target){
-            return i
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2);
+        if(nums[mid] == target){
+            return mid;
+        }
+        else if (nums[mid] < target){
+            left = mid + 1;
+        }
+        else{ 
+            right = mid - 1;
         }
     }
     return -1;
