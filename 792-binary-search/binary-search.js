233d645d64143d5f69231c0de56a156592556c88
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2025-02-07 21:23:48 +0530

    Time: 0 ms (100.00%) | Memory: 53.4 MB (8.46%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -3,43 +3,43 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
-
-    while(left <= right){
-        let mid = Math.floor((left + right) / 2);
-        if(nums[mid] == target){
-            return mid;
-        }
-        else if (nums[mid] < target){
-            left = mid + 1;
-        }
-        else{ 
-            right = mid - 1;
-        }
-    }
-    return -1;
-};
-
-
 // var search = function(nums, target) {
-//     let left = 0; 
+//     let left = 0;
 //     let right = nums.length - 1;
 
 //     while(left <= right){
 //         let mid = Math.floor((left + right) / 2);
 //         if(nums[mid] == target){
-//             return nums[mid];
+//             return mid;
 //         }
-//         else if (nums[mid] < target) {
+//         else if (nums[mid] < target){
 //             left = mid + 1;
-
 //         }
-//         else {
-//             right  = mid - 1;
+//         else{ 
+//             right = mid - 1;
 //         }
 //     }
 //     return -1;
 // };
+
+
+var search = function(nums, target) {
+    let left = 0; 
+    let right = nums.length - 1;
+
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2);
+        if(nums[mid] == target){
+            return mid
+        }
+        else if (nums[mid] < target) {
+            left = mid + 1;
+
+        }
+        else {
+            right  = mid - 1;
+        }
+    }
+    return -1;
+};
 
