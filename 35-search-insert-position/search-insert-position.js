bb37431f5c042a495149911e5d7c513b490e07ba
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2025-03-02 01:05:34 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (9.25%) - LeetSync

--- 35-search-insert-position/search-insert-position.js
+++ 35-search-insert-position/search-insert-position.js
@@ -7,17 +7,17 @@ var searchInsert = function(nums, target) {
     let l = 0; 
     let r = nums.length - 1;
 
-    while( l <= r){
-        let mid = Math.floor((l + r ) / 2);
-        if(nums[mid] == target) {
-            return mid;
+    while ( l <= r){
+        let mid = Math.floor((l + r) / 2);
+        if(nums[mid] == target){
+            return mid
         }
         else if (nums[mid] < target){
-        l = mid + 1;
+            l = mid + 1;
         }
         else {
             r = mid - 1;
         }
-    }    
-    return l;
+    }
+    return l;    
 };
