e9325ea322b0c19716b5dd86d47abfaaf9d0290c
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2025-03-04 13:04:35 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (13.33%) - LeetSync

--- 162-find-peak-element/find-peak-element.js
+++ 162-find-peak-element/find-peak-element.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    let left = 0, right = nums.length - 1;
+    let left = 0; right = nums.length - 1;
 
     while(left < right){
         let mid = left + Math.floor((right - left) / 2);
@@ -12,8 +12,9 @@ var findPeakElement = function(nums) {
             right = mid;
         }
         else {
-            left = mid + 1
+            left = mid + 1;
         }
     }
     return left;
+    
 };
