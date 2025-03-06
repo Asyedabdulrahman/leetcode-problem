74b4e98baf545d6a086af1763b75aa8cd8586d96
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2025-03-06 23:53:56 +0530

    Time: 8 ms (81.31%) | Memory: 57.5 MB (17.79%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.js
+++ 907-koko-eating-bananas/koko-eating-bananas.js
@@ -4,24 +4,24 @@
  * @return {number}
  */
 var minEatingSpeed = function(piles, h) {
-    let left = 1, right = Math.max(...piles);
+    let left = 0; right = Math.max(...piles);
     let res = right;
 
-    while(left <= right){
-        let k = (left + right) >> 1;
+    while (left <= right){
         let hours = 0;
+        let k = Math.floor((left + right) / 2);
 
-        for(let p of piles){
-            hours += Math.floor((p + k - 1) /k);
+        for(let pile of piles){
+            hours += Math.ceil(pile / k);
         }
 
         if(hours <= h){
-            res = Math.min(res,k)
+            res = Math.min(res,k);
             right = k - 1;
         }
         else {
             left = k + 1;
         }
     }
-    return res;
+    return res;    
 };
