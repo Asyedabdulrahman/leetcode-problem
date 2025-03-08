f25d47915c4d0ada58603a759a2030c7af76c817
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2025-03-09 02:23:08 +0530

    Time: 3 ms (33.70%) | Memory: 54.3 MB (25.93%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.js
+++ 410-split-array-largest-sum/split-array-largest-sum.js
@@ -1,36 +1,28 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
- */
 var splitArray = function(nums, k) {
-    let l = Math.max(...nums);
-    let r = nums.reduce((a,b) => a + b, 0 );
-    let n = nums.length;
-    if(n === k) return l;
+    let l = Math.max(...nums), r = nums.reduce((a, b) => a + b, 0);
+    const n = nums.length;
+    if (n === k) return l;
 
     let ans = 0;
-    while(l <= r){
+    while (l <= r) {
         let m = Math.floor((l + r) / 2);
-        let tempsum = 0, cnt = 0;
-        for(let i = 0; i < n; i++){
-            if(tempsum + nums[i] <= m){
-                tempsum += nums[i];
-            }
-            else {
+        let tempSum = 0, cnt = 0;
+        for (let i = 0; i < n; i++) {
+            if (tempSum + nums[i] <= m) {
+                tempSum += nums[i];
+            } else {
                 cnt++;
-                tempsum = nums[i];
+                tempSum = nums[i];
             }
         }
         cnt++;
-    
-    if(cnt <=k){
-        ans = m;
-        r = m - 1;
-    }
-    else {
-        l = m + 1;
-    }
+
+        if (cnt <= k) {
+            ans = m;
+            r = m - 1;
+        } else {
+            l = m + 1;
+        }
     }
     return ans;
 };
