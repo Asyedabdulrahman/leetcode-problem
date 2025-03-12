a6f6598f7b615bf0078b9094011e7ab5c68ef548
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2025-03-12 11:12:34 +0530

    Time: 1 ms (86.94%) | Memory: 54.2 MB (31.18%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.js
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.js
@@ -3,6 +3,7 @@
  * @return {string}
  */
 var reverseWords = function(s) {
+    if(s.length < 1) return 0; //base case
     let result = s.trim().split(/\s+/).reverse().join(" ");
     return result;
 };
