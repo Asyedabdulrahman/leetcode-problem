aa4a9834d4e72d40680b9d37f1996e4d6fa06050
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2025-08-22 01:45:38 +0530

    Time: 1 ms (91.77%) | Memory: 45 MB (54.44%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -19,7 +19,7 @@ class Solution {
         if(root == null) return result;
 
 
-        Queue<TreeNode> queue = new ArrayDeque<>(); 
+        Queue<TreeNode> queue = new LinkedList<>(); 
         queue.offer(root);
         
         while(!queue.isEmpty()){
