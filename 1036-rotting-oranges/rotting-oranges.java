1e63e272a8477b7e385bd5fc9bcb8464ab4c1afa
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2025-09-10 00:01:29 +0530

    Time: 2 ms (66.28%) | Memory: 42.4 MB (76.52%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,50 +1,39 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        // int[][] visited = grid;
+        int m = grid.length, n = grid[0].length;
         Queue<int[]> q = new ArrayDeque<>();
+        int fresh = 0;
 
-        int countFreshOrange = 0;
-        
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(grid[i][j] == 2){
-                    q.offer(new int[] {i,j});
-                }
-                if(grid[i][j] == 1){
-                    ++countFreshOrange;
-                }
+        // collect initial rotten oranges and count fresh
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) q.offer(new int[]{i, j, 0});
+                else if (grid[i][j] == 1) fresh++;
             }
         }
 
-        if(countFreshOrange == 0){
-            return 0;
-        }
-        if(q.isEmpty()){
-            return -1;
+        if (fresh == 0) return 0;        // no fresh oranges
+        if (q.isEmpty()) return -1;      // no rotten to spread
+
+        int minutes = 0;
+        int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0], y = cur[1], time = cur[2];
+            minutes = time; // track the last minute we reached
+
+            for (int[] d : dirs) {
+                int nx = x + d[0], ny = y + d[1];
+                if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
+                if (grid[nx][ny] != 1) continue;
+
+                grid[nx][ny] = 2;
+                fresh--;
+                q.offer(new int[]{nx, ny, time + 1});
+            }
         }
 
-        int minutes = -1;
-        int[][] directions = {{1,0}, {-1,0}, {0,-1}, {0,1}};
-        while(!q.isEmpty()){
-            int size = q.size();
-            while(size-- > 0){
-                int[] cell = q.poll();
-                int x = cell[0];
-                int y = cell[1];
-                for(int[] dir : directions){
-                    int i = x + dir[0];
-                    int j = y + dir[1];
-                    if(i >= 0 && i < m && j >= 0 && j < n && grid[i][j] == 1){
-                        grid[i][j] = 2;
-                        --countFreshOrange;
-                        q.offer(new int[] {i, j});
-                    }
-                }
-            }
-            minutes++;
-        }
-        return countFreshOrange == 0 ? minutes : -1;
+        return fresh == 0 ? minutes : -1;
     }
 }
