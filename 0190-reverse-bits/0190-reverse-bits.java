77b575143d5b6e1e610e791b2ff4b0e5a52e0dd8
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2026-01-13 04:21:55 +0530

    Time: 1 ms (56.36%), Space: 42.7 MB (14.06%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -5,9 +5,9 @@ class Solution {
         for(int i = 0; i < 32; i++){
             ans = ans << 1;
 
-            ans =  ans | (n & 1);
+            ans = ans | (n & 1);
 
-            n = n >>> 1;
+            n = n >> 1;
         }
         return ans;
     }
