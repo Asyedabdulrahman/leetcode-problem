378df9a3dd1fe557f145492858a9996e06cc8f40
syed abdul rahman <110740098+Asyedabdulrahman@users.noreply.github.com>
2026-04-10 01:07:15 +0530

    Time: 4 ms (63.4%), Space: 50.2 MB (60.94%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,29 +1,25 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] memo = new int[m][n];
+        int m = grid.length; 
+        int n = grid[0].length; 
 
-        for(int[] row : memo){
-            Arrays.fill(row, -1);
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+
+        for(int i = 1; i < m; i++){
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
         }
 
-        return dfs(grid, 0, 0, memo);
-    }
+        for(int j = 1; j < n; j++){
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
 
-    private int dfs(int[][] grid, int i, int j, int[][] memo) {
-        int m = grid.length;
-        int n = grid[0].length;
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
 
-        if (i >= m || j >= n) return Integer.MAX_VALUE / 2;
-
-        if (i == m - 1 && j == n - 1) return grid[i][j];
-
-        if(memo[i][j] != -1) return memo[i][j];
-
-        int down = dfs(grid, i + 1, j, memo);
-        int right = dfs(grid, i, j + 1, memo);
-
-        return memo[i][j] = grid[i][j] + Math.min(down, right);
+        return dp[m - 1][n - 1];
     }
 }
